b573c369ff7f1c134b0a467d981923a864892783
ycremar <ec2-user@ip-172-31-32-70.us-east-2.compute.internal>
2019-10-04 05:52:59 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+  
   def initialize
     super
     @all_ratings = get_ratings
@@ -11,6 +12,11 @@ class MoviesController < ApplicationController
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
+  
+  def clear
+    session.clear
+    redirect_to movies_path  
+  end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
@@ -18,18 +24,38 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
   
-  def index
-    @movies = params[:ratings].nil? ? Movie.all : Movie.where("rating IN (?)", params[:ratings].keys)
-    
-    sort = params[:sort] || session[:sort]
-    case sort
-    when 'title'
-      ordering, @title_header = {:order => :title}, 'hilite'
-      @movies = @movies.order(:title)
-    when 'release_date'
-      ordering, @date_header = {:order => :release_date}, 'hilite'
-      @movies = @movies.order(:release_date)
+  def get_sort_state
+    @sort = params[:sort]
+    if params[:sort].nil?
+      @sort =  (session[:sort].nil?) ? :unsorted : session[:sort]
     end
+    @sort
+  end
+  
+  def get_filter_state
+    @filter = session[:filter]
+    if params[:commit] == 'Refresh'
+      @filter = params[:ratings].keys unless params[:ratings].nil?
+    end
+    @filter = @all_ratings if @filter.nil?
+    @filter
+  end
+
+  def index
+    
+    session[:sort] = get_sort_state
+    session[:filter] = get_filter_state
+
+    if params[:sort].nil? or params[:filter].nil?
+      params[:ratings] = @filter
+      params[:sort] = @sort
+      redirect_to movies_path(sort: @sort, filter: @filter)
+      return
+    end
+    
+    @movies = Movie.where("rating IN (?)", @filter)
+    @movies = @movies.order(@sort) unless @sort.nil? or @sort=='unsorted'
+
   end
 
   def new
